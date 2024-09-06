0bc879407e626250475fa6d50fe5f3ca949dcca9
TypeErrorDev <79867801+TypeErrorDev@users.noreply.github.com>
2024-09-06 07:34:03 -0700

    added our database model

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 # import models
+from models import (Base, session, 
+                    Book, engine)
+
+
 # main menu - add, search, analysis, exit, view
 # add books to database
 # edit books
@@ -6,3 +10,8 @@
 # search books
 # data cleaning
 # loop runs program
+
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
--- models.py
+++ models.py
@@ -1,4 +1,27 @@
+from sqlalchemy import (create_engine, Column, 
+                        Integer, String, Date)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
 # create a database
+engine = create_engine('sqlite:///books.db',echo=False)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
 # books.db
+class Book(Base):
+    __tablename__ = "Books"
+
+    id = Column(Integer, primary_key=True)
+    title = Column('Title', String)
+    author = Column('Author', String)
+    published_date = Column("Published", Date)
+    price = Column("Price", Integer)
+    
+    def __repr__(self):
+        return f"Title: {self.title} | Author: {self.author} | Published: {self.published_date} | Price: {self.price}"
+    
+
 # create model
     # title, author, date published, price
