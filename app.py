78115505851b262dd50391c0fcf8e34c1c3f8781
TypeErrorDev <79867801+TypeErrorDev@users.noreply.github.com>
2024-09-06 15:06:42 -0700

    add book to db function

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from models import (Base, session,
                     Book, engine)
 import datetime
 import csv
+import time
 
 
 # Create the main menu - add, search, analysis, exit, view
@@ -36,16 +37,37 @@ def menu():
 def clean_date(date_str):
     months = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December']
     split_date = date_str.split(' ')
-    # mutate the string of dates into integers
-    month = int(months.index(split_date[0]) + 1)
-    day = int(split_date[1].split(',')[0])
-    year = int(split_date[2])
-    return datetime.date(year,month,day)
+    try:
+        month = int(months.index(split_date[0]) + 1)
+        day = int(split_date[1].split(',')[0])
+        year = int(split_date[2])
+        return_date = datetime.date(year,month,day)
+    except ValueError:
+        input('''
+              \n****** DATE ERROR ******
+              \rThe date format should include a valid Month Day, Year from the past.
+              \rEx: January 13, 2003
+              \rPress enter to try again.
+              \r************************''')
+        return
+    else:
+        return return_date
 
 # creates a function to turn a string price into a float price and mutate it into an int
 def clean_price(price_str):
-    price_float = float(price_str)
-    return int(price_float * 100)
+    try:
+        price_float = float(price_str)
+
+    except ValueError:
+        input('''
+              \n****** PRICE ERROR ******
+              \rThe price should be a number without a currency symbol.
+              \rEx: 10.99   
+              \rPress enter to try again.
+              \r************************''')
+        return
+    else:    
+        return int(price_float * 100)
     
 
 
@@ -54,6 +76,7 @@ def add_csv():
     with open('suggested_books.csv') as csvfile:
         data = csv.reader(csvfile)
         for row in data:
+            # validates if there are any duplicates, and ensures there is only 1 or none
             book_in_db = session.query(Book).filter(Book.title==row[0]).one_or_none()
             if book_in_db == None:
                 title = row[0]
@@ -71,7 +94,25 @@ def app():
         choice = menu()
         if choice == '1':
             # add book
-            pass
+            title = input('Title: ')
+            author = input('Author: ')
+            date_error = True
+            while date_error:
+                date = input('Published Date (Ex: October 25, 2017): ')
+                date = clean_date(date)
+                if type(date) == datetime.date:
+                    date_error = False
+            price_error = True
+            while price_error:
+                price = input("Price (Ex: 25.95): ")
+                price = clean_price(price)
+                if type(price) == int:
+                    price_error = False
+            new_book = Book(title=title, author=author,published_date=date,price=price)
+            session.add(new_book)
+            session.commit()
+            print("Book added")
+            time.sleep(1.5)
         elif choice == '2':
             # view books
             pass
@@ -90,8 +131,9 @@ def app():
 if __name__ == '__main__':
     # creates all the tables defined by the models associated with a Base object in the database specified by the engine
     Base.metadata.create_all(engine)
-    # app()
     add_csv()
+    app()
+    
 
     for book in session.query(Book):
         print(book)
