13b6ad84e2f6903751d47d99b6cad74ba18f0bd2
TypeErrorDev <79867801+TypeErrorDev@users.noreply.github.com>
2024-09-06 08:21:02 -0700

    added csv to db

--- app.py
+++ app.py
@@ -1,17 +1,97 @@
 # import models
 from models import (Base, session, 
                     Book, engine)
+import datetime
+import csv
 
 
-# main menu - add, search, analysis, exit, view
-# add books to database
+# Create the main menu - add, search, analysis, exit, view
+def menu():
+    while True:
+        # \n makes the link start on a "new line"
+        # \r is basically pressing enter on the keyboard, not necessarily a new line 
+        print('''
+            \n#### PROGRAMMING BOOKS ####
+            \r1) Add Book
+            \r2) View All Books
+            \r3) Search for Books
+            \r4) Book Analysis
+            \r5) Exit''')
+        choice = input("What would you like to do? ")
+        # this is for validating that the input is one of the options we selected
+        if choice in ['1','2','3','4','5']:
+            return choice
+        else:
+            input('''Please choose one of the options above.
+                  \rA number from 1-5.
+                  \rPress enter to try again.''')
+
+
+
+
 # edit books
 # delete books
 # search books
 # data cleaning
-# loop runs program
+def clean_date(date_str):
+    months = ['January', 'February', 'March', 'April', 'May', 'June','July', 'August', 'September', 'October', 'November', 'December']
+    split_date = date_str.split(' ')
+    # mutate the string of dates into integers
+    month = int(months.index(split_date[0]) + 1)
+    day = int(split_date[1].split(',')[0])
+    year = int(split_date[2])
+    return datetime.date(year,month,day)
+
+# creates a function to turn a string price into a float price and mutate it into an int
+def clean_price(price_str):
+    price_float = float(price_str)
+    return int(price_float * 100)
+    
 
 
 
+def add_csv():
+    with open('suggested_books.csv') as csvfile:
+        data = csv.reader(csvfile)
+        for row in data:
+            book_in_db = session.query(Book).filter(Book.title==row[0]).one_or_none()
+            if book_in_db == None:
+                title = row[0]
+                author = row[1]
+                date = clean_date(row[2])
+                price = clean_price(row[3])
+                new_book = Book(title=title, author=author,published_date=date, price=price)
+                session.add(new_book)
+        session.commit()
+
+
+def app():
+    app_running = True
+    while app_running:
+        choice = menu()
+        if choice == '1':
+            # add book
+            pass
+        elif choice == '2':
+            # view books
+            pass
+        elif choice == '3':
+            # search books
+            pass
+        elif choice == '4':
+            # analysis
+            pass
+        else:
+            print('GOODBYE')
+            app_running = False
+
+
+# this dunder main is used so that when app.py is executed *directly*, it'll run the included functions (in this case, create the engine and then run the app() function automatically on execution)
 if __name__ == '__main__':
+    # creates all the tables defined by the models associated with a Base object in the database specified by the engine
     Base.metadata.create_all(engine)
+    # app()
+    add_csv()
+
+    for book in session.query(Book):
+        print(book)
